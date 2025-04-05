03cb42c112d337f51aad384e52f7d99419bd716b
David Frischknecht <fishnet37222@gmail.com>
2025-04-05 03:33:44 -0400

    MainWindow:  Add the toolbar and associated event handler.

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "MainWindow.h"
+#include <cstdint>
 
 #ifdef __linux
 #include "resources/die16.xpm"
@@ -11,6 +12,24 @@
 #include "resources/die128.xpm"
 #include "resources/die256.xpm"
 #endif
+
+// Toolbar button bitmaps.
+#include "resources/new24.xpm"
+#include "resources/open24.xpm"
+#include "resources/save24.xpm"
+#include "resources/history24.xpm"
+#include "resources/help24.xpm"
+#include "resources/about24.xpm"
+
+enum ToolId : uint16_t
+{
+	ID_NEW = wxID_HIGHEST + 1,
+	ID_LOAD,
+	ID_SAVE,
+	ID_HIGH_SCORES,
+	ID_HOW_TO_PLAY,
+	ID_ABOUT
+};
 
 MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "FNDice", wxDefaultPosition, wxDefaultSize,
 								   wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX))
@@ -28,4 +47,32 @@ MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "FNDice", wxDefaultPositio
 	icons.AddIcon(wxIcon(die256_xpm));
 #endif
 	wxFrame::SetIcons(icons);
+	
+	m_toolBar = wxFrame::CreateToolBar(wxTB_FLAT);
+	m_toolBar->AddTool(ID_NEW, wxEmptyString, wxBitmapBundle::FromBitmap(wxBitmap(new24_xpm)), "Start a new game.");
+	m_toolBar->AddTool(ID_LOAD, wxEmptyString, wxBitmapBundle::FromBitmap(wxBitmap(open24_xpm)), "Load a saved game.");
+	m_toolBar->AddTool(ID_SAVE, wxEmptyString, wxBitmapBundle::FromBitmap(wxBitmap(save24_xpm)), "Save the current game.");
+	m_toolBar->AddSeparator();
+	m_toolBar->AddTool(ID_HIGH_SCORES, wxEmptyString, wxBitmapBundle::FromBitmap(wxBitmap(history24_xpm)), "View the high scores.");
+	m_toolBar->AddSeparator();
+	m_toolBar->AddTool(ID_HOW_TO_PLAY, wxEmptyString, wxBitmapBundle::FromBitmap(wxBitmap(help24_xpm)), "Learn how to play the game.");
+	m_toolBar->AddTool(ID_ABOUT, wxEmptyString, wxBitmapBundle::FromBitmap(wxBitmap(about24_xpm)), "View the license and copyright information.");
+	m_toolBar->Realize();
+	m_toolBar->Bind(wxEVT_TOOL, &MainWindow::ToolBar_OnTool, this);
+	m_toolBar->EnableTool(ID_SAVE, false);
+}
+
+void MainWindow::ToolBar_OnTool(wxCommandEvent& event)
+{
+	switch (const auto toolId = event.GetId())
+	{
+		case ID_NEW:
+		case ID_LOAD:
+		case ID_SAVE:
+		case ID_HIGH_SCORES:
+		case ID_HOW_TO_PLAY:
+		case ID_ABOUT:
+		default:
+			break;
+	}
 }
