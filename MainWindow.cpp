9f6b8bac7ac0e5ed1ecdd1e62517968ca081b12c
David Frischknecht <fishnet37222@gmail.com>
2025-04-05 02:36:27 -0400

    Initial commit.

---
+++ MainWindow.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) 2025 David A. Frischknecht
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "MainWindow.h"
+
+#ifdef __linux
+#include "resources/die16.xpm"
+#include "resources/die32.xpm"
+#include "resources/die64.xpm"
+#include "resources/die128.xpm"
+#include "resources/die256.xpm"
+#endif
+
+MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "FNDice", wxDefaultPosition, wxDefaultSize,
+								   wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX))
+{
+	wxFrame::SetBackgroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_FRAMEBK));
+	
+	wxIconBundle icons;
+#ifdef _WIN32
+	icons = wxIconBundle("APP_ICON", nullptr);
+#elif defined(__linux)
+	icons.AddIcon(wxIcon(die16_xpm));
+	icons.AddIcon(wxIcon(die32_xpm));
+	icons.AddIcon(wxIcon(die64_xpm));
+	icons.AddIcon(wxIcon(die128_xpm));
+	icons.AddIcon(wxIcon(die256_xpm));
+#endif
+	wxFrame::SetIcons(icons);
+}
