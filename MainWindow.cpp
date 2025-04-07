1548046caa931f0119f2da7934004acb6eab3938
Dave Frischknecht <fishnet37222@gmail.com>
2025-04-07 16:12:47 -0400

    MainWindow: Add the icon to be used on Linux.

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -4,6 +4,14 @@
 
 #include "MainWindow.h"
 
+#ifdef __linux
+#include "resources/die16.xpm"
+#include "resources/die32.xpm"
+#include "resources/die64.xpm"
+#include "resources/die128.xpm"
+#include "resources/die256.xpm"
+#endif
+
 MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "FNDice", wxDefaultPosition, wxDefaultSize,
 	wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX))
 {
@@ -12,6 +20,12 @@ MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "FNDice", wxDefaultPositio
 	wxIconBundle icons;
 #ifdef __WIN32__
 	icons = wxIconBundle("APP_ICON", nullptr);
+#elif defined(__linux)
+	icons.AddIcon(wxIcon(die16_xpm));
+	icons.AddIcon(wxIcon(die32_xpm));
+	icons.AddIcon(wxIcon(die64_xpm));
+	icons.AddIcon(wxIcon(die128_xpm));
+	icons.AddIcon(wxIcon(die256_xpm));
 #endif
 	wxFrame::SetIcons(icons);
 
