d82f49b9569d551f9985dfab56a0e66423709cae
David Frischknecht <fishnet37222@gmail.com>
2026-05-20 21:12:39 -0400

    Refactor CMakeLists.txt for better source management and update minimum CMake version; enhance MainWindow constructor for improved icon handling on Linux

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -4,11 +4,33 @@
 
 #include "MainWindow.h"
 
-MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "FNDice", wxDefaultPosition, wxDefaultSize,
-	wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX))
+#ifdef __linux__
+#include "bitmaps/die128.xpm"
+#include "bitmaps/die16.xpm"
+#include "bitmaps/die256.xpm"
+#include "bitmaps/die32.xpm"
+#include "bitmaps/die64.xpm"
+#endif
+
+MainWindow::MainWindow()
+	: wxFrame(nullptr, wxID_ANY, "FNDice", wxDefaultPosition, wxDefaultSize,
+			  wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX))
 {
 	wxFrame::SetBackgroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_FRAMEBK));
-	wxFrame::SetIcons(wxIconBundle("APP_ICON", nullptr));
+
+	wxIconBundle icons;
+
+#ifdef _WINDOWS
+	icons = wxIconBundle("APP_ICON", nullptr);
+#elif defined(__linux__)
+	icons.AddIcon(wxIcon(die16_xpm));
+	icons.AddIcon(wxIcon(die32_xpm));
+	icons.AddIcon(wxIcon(die64_xpm));
+	icons.AddIcon(wxIcon(die128_xpm));
+	icons.AddIcon(wxIcon(die256_xpm));
+#endif
+
+	wxFrame::SetIcons(icons);
 
 	CenterOnScreen();
 }
